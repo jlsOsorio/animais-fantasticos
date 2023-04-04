1c08be8593c31ed5e20f79de3da0ffcabe59bfa1
Joao Osorio <joao.l.osorio@gmail.com>
2023-04-04 20:19:35 +0100

    Refactored dropdown-menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,42 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+    this.activeClass = 'active';
 
-  function handleClick(event) {
+    // Definie o touchstart e click como argumento padrão de events caso o utilizador não defina
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else {
+      this.events = events;
+    }
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  // Activa o dropdownmenu e adiciona a função que observa o clique dentro dele
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add("active");
-    outsideClick(this, ["touchstart", "click"], () => {
-      this.classList.remove("active");
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  // Adiciona os eventos ao dropdownmenu
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  }); // O touchstart é igual ao click. Por padrão o click demora 300ms no engine mobile para ser executado. O touchstart começa na hora
+  }
+  
+  init() {
+    if (this.dropdownMenus) {
+      this.addDropdownMenusEvent();
+    }
+
+    return this;
+  }
 }
